214641a0a5e35d7854f50288870d6810b03d2d9b
Ojas Bendale <ojasbendale18200@gmail.com>
2022-01-27 22:04:19 +0530

    Day 1

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14  bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>Excitement</p>
+        <p>New Chapters</p>
+        <p>Self Care</p>
+        <p>Pomodoro</p>
+        <p>New Day</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p>I love words</p>
+        <p>Snow is cool</p>
+        <p>Rainy day</p>
+        <p>Learning</p>
+        <p>Always</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Host</h5>
+        <p>Everton is a good team</p>
+        <p>Premier League On</p>
+        <p>Lowe Down</p>
+        <p>October</p>
+        <p>Liquid</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Support</h5>
+        <p>Site Tree Link</p>
+        <p>Links to Clone</p>
+        <p>Super Site</p>
+        <p>Skylab Productions</p>
+        <p>Checkout my resume</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
